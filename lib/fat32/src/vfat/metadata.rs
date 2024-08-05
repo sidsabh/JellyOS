2f76a81114d73106ded430605f894bc28479ce9c
Sid Sabhnani <ssabhnani@utexas.edu>
2024-08-04 19:31:46 -0700

    metadata adventure

--- lib/fat32/src/vfat/metadata.rs
+++ lib/fat32/src/vfat/metadata.rs
@@ -2,7 +2,8 @@ use core::fmt;
 
 use alloc::string::String;
 
-use crate::traits;
+use crate::traits::{self, Timestamp as TimestampTrait, Metadata as MetadataTrait};
+
 
 /// A date as represented in FAT32 on-disk structures.
 #[repr(C, packed)]
@@ -29,11 +30,111 @@ pub struct Timestamp {
 /// Metadata for a directory entry.
 #[derive(Default, Debug, Clone)]
 pub struct Metadata {
-    // FIXME: Fill me in.
+    pub attributes: Attributes,
+    pub created_time: Time,
+    pub created_date: Date,
+    pub accessed_date: Date,
+    pub modified_time: Time,
+    pub modified_date: Date,
 }
 
 // FIXME: Implement `traits::Timestamp` for `Timestamp`.
+impl traits::Timestamp for Timestamp {
+    fn year(&self) -> usize {
+        // Bits 15-9 represent the year offset from 1980
+        let val = (self.date.0 >> 9) & 0x7F;
+        (val as usize) - 1980
+    }
+
+    fn month(&self) -> u8 {
+        // Bits 8-5 represent the month
+        ((self.date.0 >> 5) & 0xF) as u8
+    }
+
+    fn day(&self) -> u8 {
+        // Bits 4-0 represent the day
+        (self.date.0 & 0x1F) as u8
+    }
+
+    fn hour(&self) -> u8 {
+        // Bits 15-11 represent the hour
+        ((self.time.0 >> 11) & 0x1F) as u8
+
+    }
+
+    fn minute(&self) -> u8 {
+        // Bits 10-5 represent the minutes
+        ((self.time.0 >> 5) & 0x1F) as u8
+    }
+
+    fn second(&self) -> u8 {
+        // Bits 4-0 represent the seconds divided by 2
+        (2 * (self.time.0 & 0x1F)) as u8 
+    }
+}
+
+impl fmt::Display for Timestamp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{:02}:{:02}:{:02} on {:02}/{:02}/{:04}",
+            self.hour(),
+            self.minute(),
+            self.second(),
+            self.month(),
+            self.day(),
+            self.year()
+        )
+    }
+}
 
 // FIXME: Implement `traits::Metadata` for `Metadata`.
+impl traits::Metadata for Metadata {
+    type Timestamp = Timestamp;
+
+    fn read_only(&self) -> bool {
+        self.attributes.0 == 0x01
+    }
+
+    fn hidden(&self) -> bool {
+        self.attributes.0 == 0x02
+    }
+
+    fn created(&self) -> Self::Timestamp {
+        Timestamp {
+            date: self.created_date,
+            time: self.created_time,
+        }
+    }
+
+    fn accessed(&self) -> Self::Timestamp {
+        Timestamp {
+            date: self.accessed_date,
+            time: Time(0), // no info, conservatively default to 00:00:00
+        }
+    }
+
+    fn modified(&self) -> Self::Timestamp {
+        Timestamp {
+            date: self.modified_date,
+            time: self.modified_time,
+        }
+    }
+}
 
 // FIXME: Implement `fmt::Display` (to your liking) for `Metadata`.
+impl fmt::Display for Metadata {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Created @ {}\nLast Modified @ {}\nLast Accessed on {:02}/{:02}/{:04}\nHidden: {}\nRead-Only: {}",
+            self.created(),
+            self.modified(),
+            self.accessed().day(),
+            self.accessed().month(),
+            self.accessed().year(),
+            self.hidden(),
+            self.read_only()
+        )
+    }
+}
