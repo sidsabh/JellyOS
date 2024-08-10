43613bf46aa0ac87e454e4fa8da0c031f732f3b3
Sid Sabhnani <ssabhnani@utexas.edu>
2024-08-09 23:07:46 -0700

    bootloader reads fs now

--- kern/src/allocator.rs
+++ kern/src/allocator.rs
@@ -80,19 +80,21 @@ use crate::allocator::util::align_up;
 ///
 /// This function is expected to return `Some` under all normal cirumstances.
 pub fn memory_map() -> Option<(usize, usize)> {
-    let page_size = 1 << 12;
-    let mut binary_end = unsafe { (&__text_end as *const u8) as usize };
-    binary_end = align_up(binary_end, page_size);
+    
+    // let page_size: usize = 1 << 12;
+    // let mut binary_end = unsafe { (&__text_end as *const u8) as usize };
+    // binary_end = align_up(binary_end, page_size);
+    // let mut atags = Atags::get();
+    // match atags.find(|tag| tag.mem().is_some()) {
+    //     Some(atag) => {
+    //         let mem = atag.mem().unwrap();
+    //         // kprintln!("{} {}", binary_end, (mem.size as usize)-binary_end);
+    //         Some((binary_end, (mem.size as usize) - binary_end))
+    //     }
+    //     None => None,
+    // }
+    Some((610304, 1006022656))
 
-    let mut atags = Atags::get();
-    match atags.find(|tag| tag.mem().is_some()) {
-        Some(atag) => {
-            let mem = atag.mem().unwrap();
-            // kprintln!("{} {}", binary_end, (mem.size as usize)-binary_end);
-            Some((binary_end, (mem.size as usize) - binary_end))
-        }
-        None => None,
-    }
 }
 
 impl fmt::Debug for Allocator {
